93b07a7f2f5b9935adb3ca6b1624cf6d3a1a223b
Francislie <mfrancislie@gmail.com>
2023-08-30 02:25:31 -0700

    SubmitBtn-complete

--- src/components/SubmitBtn.jsx
+++ src/components/SubmitBtn.jsx
@@ -1,8 +1,23 @@
+import { useNavigate } from 'react-router-dom';
+
 /* eslint-disable react/prop-types */
 const SubmitBtn = ({ text }) => {
+  const navigate = useNavigate();
+  const isSubmitting = navigate.isSubmitting === 'submitting';
+
   return (
-    <button type="submit" className="btn btn-primary btn-block">
-      {text}
+    <button
+      type="submit"
+      className="btn btn-primary btn-block"
+      disabled={isSubmitting}
+    >
+      {isSubmitting ? (
+        <>
+          <span className="loading loading-ring loading-md"></span>
+        </>
+      ) : (
+        text || 'submit'
+      )}
     </button>
   );
 };
