34aa40ac9e4f11c7d9666fb9bb5c2405abb5b066
Francislie <mfrancislie@gmail.com>
2023-08-30 00:57:30 -0700

    Configure react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable no-unused-vars */
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import {
   HomeLayout,
   Landing,
@@ -13,7 +14,55 @@ import {
   Orders,
 } from './pages';
 function App() {
-  return <h1 className="text-7xl font-bold underline">Tailwind project</h1>;
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <HomeLayout />,
+      errorElement: <Error />,
+      children: [
+        {
+          index: true,
+          element: <Landing />,
+        },
+        {
+          path: 'products',
+          element: <Products />,
+        },
+        {
+          path: 'products/:id',
+          element: <SingleProduct />,
+        },
+        {
+          path: 'cart',
+          element: <Cart />,
+        },
+        {
+          path: 'about',
+          element: <About />,
+        },
+        {
+          path: 'checkout',
+          element: <Checkout />,
+        },
+        {
+          path: 'orders',
+          element: <Orders />,
+        },
+      ],
+    },
+    {
+      path: '/login',
+      element: <Login />,
+      errorElement: <Error />,
+    },
+    {
+      path: '/register',
+      element: <Register />,
+      errorElement: <Error />,
+    },
+  ]);
+
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
