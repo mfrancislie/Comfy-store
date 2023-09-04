7a10f5d995114fd2f33025a012b1e8347bb4559a
Francislie <mfrancislie@gmail.com>
2023-09-04 04:31:36 -0700

    implement checkout page

--- src/App.jsx
+++ src/App.jsx
@@ -19,10 +19,12 @@ import { loader as landingLoader } from './pages/Landing';
 import { loader as singleProductLoader } from './pages/SingleProduct';
 import { loader as productLsoader } from './pages/Products';
 import { ErrorElement } from './components';
+import { loader as checkoutLoader } from './pages/Checkout';
 
 // actions
 import { action as registerAction } from './pages/Register';
 import { action as loginAction } from './pages/Login';
+import { action as checkoutAction } from './components/CheckoutForm';
 import { store } from './store';
 
 function App() {
@@ -61,6 +63,8 @@ function App() {
         {
           path: 'checkout',
           element: <Checkout />,
+          loader: checkoutLoader(store),
+          action: checkoutAction(store),
         },
         {
           path: 'orders',
