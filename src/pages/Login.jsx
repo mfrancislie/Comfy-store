7973e9bebe306f006444ab82d257efab3d3b364f
Francislie <mfrancislie@gmail.com>
2023-09-04 03:58:53 -0700

    Demo user

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,10 @@
 /* eslint-disable react-refresh/only-export-components */
-import { Form, Link, redirect } from 'react-router-dom';
+import { Form, Link, redirect, useNavigate } from 'react-router-dom';
 import { FormInput, SubmitBtn } from '../components';
 import { loginUser } from '../features/user/userSlice';
 import { toast } from 'react-toastify';
 import { customFetch } from '../utils';
+import { useDispatch } from 'react-redux';
 
 export const action =
   (store) =>
@@ -28,6 +29,23 @@ export const action =
   };
 
 const Login = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const loginAsGuestUser = async () => {
+    try {
+      const response = await customFetch.post('/auth/local', {
+        identifier: 'test@test.com',
+        password: 'secret',
+      });
+      dispatch(loginUser(response.data));
+      toast.success('welcome guest user');
+      navigate('/');
+    } catch (error) {
+      console.log(error);
+      toast.error('guest user login error.please try later.');
+    }
+  };
+
   return (
     <section className="h-screen grid place-items-center">
       <Form
@@ -40,7 +58,11 @@ const Login = () => {
         <div className="mt-4">
           <SubmitBtn text="login" />
         </div>
-        <button type="button" className="btn btn-secondary btn-block">
+        <button
+          type="button"
+          className="btn btn-secondary btn-block"
+          onClick={loginAsGuestUser}
+        >
           guest user
         </button>
         <p className="text-center">
